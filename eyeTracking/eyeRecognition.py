d6c95ebf98a990808a27b6b4cb036269ce719eb4
Cathy Jiao <cathy.jiao@gmail.com>
2018-01-13 22:59:30 -0800

    added calibration to nose detection

--- eyeTracking/eyeRecognition.py
+++ eyeTracking/eyeRecognition.py
@@ -1,41 +1,58 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FPS, 10)
+def run(x, y, w, h):
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FPS, 30)
 
-nose_cascade = cv2.CascadeClassifier('haarcascade_mcs_nose.xml')
+    nose_cascade = cv2.CascadeClassifier('haarcascade_mcs_nose.xml')
+    cp = [(x+w)/2, (y+h)/2]
+    while (cap.isOpened()):
 
-while(cap.isOpened()):
+        ret, frame = cap.read()
 
-    ret, frame = cap.read()
+        if ret:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            nose = nose_cascade.detectMultiScale(gray, 1.3, 5)
 
-    if ret:
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        nose = nose_cascade.detectMultiScale(gray, 1.3, 5)
+            for (ex, ey, ew, eh) in nose:
+                if((abs(((ex+ew)/2)-cp[0]) > 2 or abs(((ey+eh)/2)-cp[1]) > 2) or (abs(((ex+ew)/2)-cp[0]) < 20 or abs(((ey+eh)/2)-cp[1]) < 20)):
+                    cp = [(ex+ew)/2, (ey+eh)/2]
 
-        sumx = 0
-        sumy = 0
-        sumew =0
-        sumeh =0
-        for (ex, ey, ew, eh) in nose:
-            if(ex != sumx):
-                sumx = ex
-                sumy = ey
-            sumew = sumew + ew
-            sumeh = sumeh + eh
+            print(cp[0], cp[1])
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    cap.release()
+    cv2.destroyAllWindows()
 
-        sumx = int(sumx)
-        sumy = int(sumy)
-        sumew = int(sumew)
-        sumeh = int(sumeh)
+if __name__ == "__main__":
+    cap = cv2.VideoCapture(0)
+    cap.set(cv2.CAP_PROP_FPS, 10)
 
-        cv2.rectangle(frame,(sumx,sumy),(sumx+sumew, sumy+sumeh),(0,255,0),2)
-        roi_gray2 = gray[sumy:sumy + sumeh, sumx:sumx + sumew]
-        roi_color2 = frame[sumy:sumy + sumeh, sumx:sumx + sumew]
-        print(sumx, sumy)
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-cap.release()
-cv2.destroyAllWindows()
+    nose_cascade = cv2.CascadeClassifier('haarcascade_mcs_nose.xml')
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+
+        if ret:
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            nose = nose_cascade.detectMultiScale(gray, 1.3, 5)
+
+            nx = 0
+            ny = 0
+            nw = 0
+            nh = 0
+            for (x, y, w, h) in nose:
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                nx = x
+                ny = y
+                nw = w
+                nh = h
+
+            cv2.imshow('frame', frame)
+            if cv2.waitKey(1) & 0xFF == ord('e'):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    run(nx, ny, nw, nh)
